f667e603ffa98ca7364e363e0cb2560f353f3091
Darrell Tang <TangDa01@risk.regn.net>
2022-02-22 20:53:48 -0800

    chezmoi initial nvchad commit

---
+++ dot_config/nvim/lua/custom/init.lua
@@ -0,0 +1,17 @@
+-- Please check NvChad docs if you're totally new to nvchad + dont know lua!!
+-- This is an example init file in /lua/custom/
+vim.o.incsearch = true
+vim.o.foldenable = false
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.autoread = true
+
+-- this init.lua can load stuffs etc too so treat it like your ~/.config/nvim/
+
+-- MAPPINGS
+local map = require("core.utils").map
+
+map("n", "<leader>cc", ":Telescope <CR>")
+map("n", "<leader>q", ":q <CR>")
+
+-- NOTE: the 4th argument in the map function is be a table i.e options but its most likely un-needed so dont worry about it
---
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,8 @@
+-- Example plugins file!
+-- (suggestion) -> lua/custom/plugins/init.lua or anywhere in custom dir
+
+return {
+   { "tpope/vim-fugitive", cmd = "G"},
+   { "pedrohdz/vim-yaml-folds", ft = {"yaml","yml"}},
+   { 'folke/which-key.nvim' }, 
+}
