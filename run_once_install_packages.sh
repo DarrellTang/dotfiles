f9b1607e0abf161e6dee2357611acd6aa878294a
Darrell Tang <darrelltang@gmail.com>
2022-05-07 16:41:32 -0700

    add chezmoi to install, clone nvchad

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install coreutils curl fzf krew kubectx kubernetes-cli kubernetes-helm nvim openssl screenfetch tfenv terraform tldr tmux unzip zsh 
+brew install coreutils chezmoi curl fzf krew kubectx kubernetes-cli kubernetes-helm nvim openssl screenfetch tfenv terraform tldr tmux unzip zsh 
 brew install --cask alacritty lens visual-studio-code slack workflowy 
+
 
