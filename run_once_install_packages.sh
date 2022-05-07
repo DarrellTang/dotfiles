d37967db85f98ff8ba31ea40d59fc71c105c4922
Darrell Tang <darrelltang@gmail.com>
2022-05-07 16:35:08 -0700

    install tfenv first before terraform

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install coreutils curl fzf krew kubectx kubernetes-cli kubernetes-helm nvim openssl screenfetch terraform tfenv tldr tmux unzip zsh 
+brew install coreutils curl fzf krew kubectx kubernetes-cli kubernetes-helm nvim openssl screenfetch tfenv terraform tldr tmux unzip zsh 
 brew install --cask alacritty lens visual-studio-code slack workflowy 
 
