121937b1c561a65b1444c083a40d3e87f7aa5472
Darrell Tang <darrelltang@gmail.com>
2022-05-06 22:10:01 -0700

    remove exa from install

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install coreutils curl exa fzf krew kubectx kubernetes-cli kubernetes-helm nvim openssl screenfetch terraform tfenv tldr tmux unzip zsh 
+brew install coreutils curl fzf krew kubectx kubernetes-cli kubernetes-helm nvim openssl screenfetch terraform tfenv tldr tmux unzip zsh 
 brew install --cask alacritty lens virtualbox visual-studio-code slack workflowy 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
