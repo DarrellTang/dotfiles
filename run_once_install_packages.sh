970ae635c68018a6c728f2c1fd5a9074f38cceba
Darrell Tang <darrelltang@gmail.com>
2022-05-07 17:42:35 -0700

    remove nvchad from external, manually clone repo first

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install coreutils chezmoi curl fzf krew kubectx kubernetes-cli kubernetes-helm nvim openssl screenfetch tfenv terraform tldr tmux unzip zsh 
 brew install --cask alacritty lens visual-studio-code slack workflowy 
