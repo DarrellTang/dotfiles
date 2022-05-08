32180dbf766c432130b816156f4ec2a2722f5608
Darrell Tang <darrelltang@gmail.com>
2022-05-07 23:29:16 -0700

    reorder nvchad setup

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+mv ~/.config/nvim ~/.config/nvim.backup
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+cp -r ~/.config/nvim.backup/lua ~/.config/nvim/lua
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install coreutils chezmoi curl fzf krew kubectx kubernetes-cli kubernetes-helm nvim openssl screenfetch tfenv terraform tldr tmux unzip zsh 
 brew install --cask alacritty lens visual-studio-code slack workflowy 
