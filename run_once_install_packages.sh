5b604ead10e62595f3960b03296d6f3bc3d01ada
Darrell Tang <darrelltang@gmail.com>
2022-05-08 00:31:08 -0700

    reapply nvchad customizations; install font

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 mv ~/.config/nvim ~/.config/nvim.backup
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
-cp -r ~/.config/nvim.backup/lua ~/.config/nvim/lua
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install coreutils chezmoi curl fzf krew kubectx kubernetes-cli kubernetes-helm nvim openssl screenfetch tfenv terraform tldr tmux unzip zsh 
-brew install --cask alacritty lens visual-studio-code slack workflowy 
+brew install --cask alacritty lens visual-studio-code slack workflowy font-meslo-lg-nerd-font
+chezmoi apply
+nvim +PackerSync
+
 
 
