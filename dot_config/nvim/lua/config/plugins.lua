3c61ddf116e47ab455d179a45d63992ce1eebcb1
Darrell Tang <darrelltang@gmail.com>
2022-09-12 08:44:47 -0700

    finalize nvim config

--- dot_config/nvim/lua/config/plugins.lua
+++ dot_config/nvim/lua/config/plugins.lua
@@ -99,6 +99,9 @@ return packer.startup(function(use)
   -- Indentlines (guides for indentation)
   use "lukas-reineke/indent-blankline.nvim"
 
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
--- dot_config/nvim/lua/config/telescope.lua
+++ dot_config/nvim/lua/config/telescope.lua
@@ -3,8 +3,6 @@ if not status_ok then
   return
 end
 
-telescope.load_extension('media_files')
-telescope.load_extension('fzf')
 
 local actions = require "telescope.actions"
 
@@ -88,14 +86,19 @@ telescope.setup {
     -- }
     -- Now the picker_config_key will be applied every time you call this
     -- builtin picker
+    find_files = {
+      search_dirs = { "/Users/dtang/.config/", "/Users/dtang/codeWorkspace/" },
+      hidden = true
+    }
   },
   extensions = {
-    media_files = {
-        -- filetypes whitelist
-        -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-        filetypes = {"png", "webp", "jpg", "jpeg"},
-        find_cmd = "rg" -- find command (defaults to `fd`)
-      }
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
     -- Your extension configuration goes here:
     -- extension_name = {
     --   extension_config_key = value,
@@ -103,3 +106,5 @@ telescope.setup {
     -- please take a look at the readme of the extension you want to configure
   },
 }
+
+telescope.load_extension('fzf')
