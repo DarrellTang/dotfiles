4872c2c769403693af37326a68033b36a89e965f
Darrell Tang <darrelltang@gmail.com>
2022-11-12 10:51:39 -0800

    delete old nvchad configs

--- dot_config/nvim/lua/custom/plugins.lua
+++
@@ -1,8 +0,0 @@
--- Example plugins file!
--- (suggestion) -> lua/custom/plugins/init.lua or anywhere in custom dir
-
-return {
-   [ "tpope/vim-fugitive"] = {cmd = "G"},
-   [ "pedrohdz/vim-yaml-folds" ] = {ft = {"yaml","yml"}},
-   [ 'folke/which-key.nvim' ] = {}
-}
