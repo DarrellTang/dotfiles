5fa86311a047cc5ad7496a960e48d15ff5048ed9
Darrell Tang <darrelltang@gmail.com>
2024-07-28 17:48:12 -0700

    add gh cli

--- run_once_01_install_packages.sh
+++ run_once_01_install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap homebrew/cask-fonts
-brew install coreutils chezmoi curl fzf krew kubectx kubernetes-cli k9s helm nvim openssl screenfetch tfenv tldr tmux lsd yh yq fd rg jq node wget
+brew install coreutils chezmoi curl fzf krew kubectx kubernetes-cli k9s helm nvim openssl screenfetch tfenv tldr tmux lsd yh yq fd rg jq node wget gh
 brew install --cask alacritty visual-studio-code slack docker obsidian font-meslo-lg-nerd-font
 nvim +PackerSync
